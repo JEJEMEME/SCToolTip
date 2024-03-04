c21e89d17e3c8b0eb5e90bca63b0a23179313efc
Tabber <tabber@jejememe.com>
2024-03-04 12:25:04 +0900

    Initial Commit

---
+++ Tests/SCToolTipTests/SCToolTipTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import SCToolTip
+
+final class SCToolTipTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
